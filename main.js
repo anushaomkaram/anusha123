d266b1a92c3955813a3ea12903ece7cdf61ac241
anusha123 <anusha.omkaram@gmail.com>
2018-04-04 10:35:29 +0530

    added1

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 
 function loadJSON(file,callback){
-var xhr=new XMLHttpRequest();
-xhr.overrideMimeType("application/json");
-xhr.open("GET",file,true);
-xhr.onreadystatechange=function(){
-  if(xhr.readyState ===4 && xhr.status=="200"){
-  callback(xhr.responseText);
-}
+var abc=new XMLHttpRequest();
+abc.overrideMimeType("application/json");
+abc.open("GET",file,true);
+abc.onreadystatechange=function(){
+  if(abc.readyState===4 && abc.status=="200"){
+    callback(abc.responseText);
+  }
 };
-xhr.send();
+abc.send();
 }
-
 loadJSON("data.json",function(text){
 let data=JSON.parse(text);
 console.log(data);
+carrer(data.carrer);
+education(data.education);
 })
+var right=document.querySelector(".content");
+function carrer(car){
+console.log(car.info);
+var h3=document.createElement("h3");
+h3.textContent="Carrer Objective";
+right.appendChild(h3);
+var hr=document.createElement("hr");
+right.appendChild(hr);
+var p=document.createElement("p");
+ p.textContent=car.info;
+ right.appendChild(p);
+}
+function education(edu){
+ var h3=document.createElement("h3");
+ h3.textContent="EDUCATION";
+ right.appendChild(h3);
+ var hr=document.createElement("hr");
+ right.appendChild(hr);
+}
